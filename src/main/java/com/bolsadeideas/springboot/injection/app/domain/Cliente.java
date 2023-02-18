c444b81f082c226c0323ae2f9892639eaf11b833
Alvarogtz <alvaro.zabala@hotmail.es>
2023-02-18 18:49:36 +0100

    Inyeccion de dependencias - Fin clase

--- src/main/java/com/bolsadeideas/springboot/injection/app/domain/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/injection/app/domain/Cliente.java
@@ -2,8 +2,11 @@ package com.bolsadeideas.springboot.injection.app.domain;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Component
+@RequestScope
 public class Cliente {
 
 	@Value("${cliente.nombre}")
--- src/main/java/com/bolsadeideas/springboot/injection/app/domain/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/injection/app/domain/Factura.java
@@ -1,20 +1,41 @@
 package com.bolsadeideas.springboot.injection.app.domain;
 
+import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 @Component
-public class Factura {
+@RequestScope
+//@SessionScope
+public class Factura implements Serializable{
 
+	private static final long serialVersionUID = 8779449246247974821L;
 	@Value("${factura.descripcion}")
 	private String descripcion;
 	@Autowired
 	private Cliente cliente;
 	@Autowired
 	private List<ItemFactura> items;
+	
+	@PostConstruct
+	public void init() {
+		cliente.setNombre(cliente.getNombre() + " ZG");
+		descripcion += " del cliente: " + cliente.getNombre();
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Factura destuida " + descripcion);
+	}
 
 	public String getDescripcion() {
 		return descripcion;
